044b86486d3feaa4d3e9ad61ab41c1fa1682cadb
zcouts <50452164+zcouts@users.noreply.github.com>
2021-03-04 17:14:34 -0500

    Create user.js
    creating user.js file

---
+++ models/user.js
@@ -0,0 +1,32 @@
+const Mongoose = require('mongoose');
+
+module.exports = Mongoose.model('User', new Mongoose.Schema({
+    firstName: {type: String, required: true},
+    lastName: {type: String, required: true},
+    age: {
+        type: Number,
+        required: true,
+        min: 0
+    },
+    ssnumber {
+        type: String,
+        required: true,
+        unique: true,
+        min: 11,
+        max: 11
+    }
+    address: {type: String, required: false},
+    phone: {
+        type: String,
+        required: false,
+        unique: true,
+        min: 10,
+        max: 10,
+
+    }
+}, {
+    toJSON: {
+        getters: true,
+        virtuals: false,
+    },
+}));
